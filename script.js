5e5c139e60b10ef21402df069dc3582df2606724
GJFresnoBDS <148788655+GJFresnoBDS@users.noreply.github.com>
2023-11-29 17:49:58 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// Lista de personas
+const personas = ['ale', 'dami', 'gero', 'gus', 'nacho', 'cave', 'juanma', 'juanpi', 'juli', 'lean', 'lucas', 'marce', 'marian', 'mati', 'santi', 'tomi'];
+
+// Función para seleccionar una persona aleatoria
+function seleccionarPersona() {
+    const indiceAleatorio = Math.floor(Math.random() * personas.length);
+    return personas[indiceAleatorio];
+}
+
+// Función para manejar el clic en el botón
+function handleClick() {
+    const selectedPerson = seleccionarPersona();
+    document.getElementById('selectedPerson').innerText = `Persona seleccionada: ${selectedPerson}`;
+}
+
+// Asociar la función al evento de clic en el botón
+document.getElementById('selectButton').addEventListener('click', handleClick);
